95712ba920ece2e643da09f554d4d9a43776ffdc
Ayarachi Fuentes Hector Daniel <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2025-10-07 11:57:05 -0300

    Add files via upload

---
+++ Menu-tab-1.0.0/menubar/config.js
@@ -0,0 +1,21 @@
+export const STORAGE_KEYS = [
+  'tiles',
+  'engine',
+  'theme',
+  'bgData',
+  'bgUrl',
+  'userName',
+  'weatherCity',
+  'gradient',
+  'panelOpacity',
+  'panelBlur',
+  'randomBg',
+  'trash',
+  'autoSync',
+  'doodle'
+];
+
+export const API_URLS = {
+  GEOCODING: 'https://geocoding-api.open-meteo.com/v1/search',
+  WEATHER: 'https://api.open-meteo.com/v1/forecast'
+};
